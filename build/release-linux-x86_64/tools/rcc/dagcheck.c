a23d0e3b5ec01ef910d102927c417bd10a3d44e4
Joey Herguth <oitzujoey@gmail.com>
2020-07-10 00:16:07 -0700

    Add a description to README.md

--- build/release-linux-x86_64/tools/rcc/dagcheck.c
+++ build/release-linux-x86_64/tools/rcc/dagcheck.c
@@ -6,7 +6,7 @@ typedef Node NODEPTR_TYPE;
 #define STATE_LABEL(p)  ((p)->x.state)
 #define PANIC	   error
 /*
-generated at Fri Jul 10 00:08:10 2020
+generated at Fri Jul 10 00:14:04 2020
 by lburg.c - faked rcsid
 */
 static void _kids(NODEPTR_TYPE, int, NODEPTR_TYPE[]);
