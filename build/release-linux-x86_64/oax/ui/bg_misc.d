12c93cdf55974ebc26a9fcd8dd3031d7c4ff9f67
Joey Herguth <oitzujoey@gmail.com>
2020-07-13 00:01:59 -0700

    Remove build directory from git

--- build/release-linux-x86_64/oax/ui/bg_misc.d
+++
@@ -1,6 +0,0 @@
-build/release-linux-x86_64/oax/ui/bg_misc.o: code/game/bg_misc.c \
- code/game/../qcommon/q_shared.h code/game/../qcommon/q_platform.h \
- code/game/../qcommon/surfaceflags.h code/game/bg_public.h
-build/release-linux-x86_64/oax/ui/bg_misc.asm: code/game/bg_misc.c \
- code/game/../qcommon/q_shared.h code/game/../qcommon/q_platform.h \
- code/game/../qcommon/surfaceflags.h code/game/bg_public.h
